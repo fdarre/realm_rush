ac7048ebed13846a6957f31c2d2b25510aec0e3c
Frédéric Darré <frederic.darre@outlook.com>
2020-07-27 10:44:06 +0200

    Update cube TextMesh label with coordinates

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class CubeEditor : MonoBehaviour
 {
     [SerializeField] [Range(1f,20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
 
     // Update is called once per frame
     void Update()
@@ -13,8 +16,14 @@ public class CubeEditor : MonoBehaviour
         Vector3 snapPos;
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-       
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        //Get component in children of type TextMesh.
+        //Attention ça ne prendra pas forcement celui qu'on veut si il y en a plusieurs.
+        //Ici ok car un seul composant TextMest sur tte la hierarchie
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x/10f + "," + snapPos.z/10f;
+
+
     }
 }

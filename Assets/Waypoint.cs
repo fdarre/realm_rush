5915bfa4df0abdcf2bb11f9cf81db37e3e0acf61
Frédéric Darré <frederic.darre@outlook.com>
2020-08-10 16:17:56 +0200

    Add Waypoint script

---
+++ Assets/Waypoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    Vector2Int gridPos;
+
+    const int GRID_SIZE = 10;
+
+    public int GetGridSize()
+    {
+        return GRID_SIZE;
+    }
+
+    public Vector2Int GetGridPosition()
+    {
+        return new Vector2Int(
+            Mathf.RoundToInt(transform.position.x / GRID_SIZE),
+            Mathf.RoundToInt(transform.position.z / GRID_SIZE)
+        );
+        
+       
+    }
+}

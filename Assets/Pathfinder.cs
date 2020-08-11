6b981b44c99d5a23ded8317beca2f0eb7ef2beb2
Frédéric Darré <frederic.darre@outlook.com>
2020-08-11 13:20:06 +0200

    Add pathfinder script

---
+++ Assets/Pathfinder.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+
+    Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+    }
+
+    private void LoadBlocks()
+    {
+        //return every GameObject with a waypoint script attached
+        var waypoints = FindObjectsOfType<Waypoint>();
+        
+        foreach (Waypoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPosition();
+
+            if (grid.ContainsKey(gridPos))
+            {
+                print("Skipping overlapping block");
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+        print("Loaded " + grid.Count + " blocks");
+    }
+
+}

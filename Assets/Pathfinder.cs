bbfe363c3f344016823cc37ac18ed679038c2189
Frédéric Darré <frederic.darre@outlook.com>
2020-08-11 21:25:59 +0200

    Expose start and end waypoints with different top colors

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -6,18 +6,21 @@ public class Pathfinder : MonoBehaviour
 {
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    [SerializeField] Waypoint startWaypoint;
+    [SerializeField] Waypoint endWaypoint;
 
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
+        ColorStartAndEnd();
     }
 
     private void LoadBlocks()
     {
         //return every GameObject with a waypoint script attached
         var waypoints = FindObjectsOfType<Waypoint>();
-        
+
         foreach (Waypoint waypoint in waypoints)
         {
             var gridPos = waypoint.GetGridPosition();
@@ -31,7 +34,12 @@ public class Pathfinder : MonoBehaviour
                 grid.Add(gridPos, waypoint);
             }
         }
-        print("Loaded " + grid.Count + " blocks");
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWaypoint.SetTopColor(Color.red);
+        endWaypoint.SetTopColor(Color.blue);
     }
 
 }
